1c46963d9b81996e4c618c3544d4276dbb0f4028
MANOJ  TADELA <manojkumartadela333@gmail.com>
2022-05-05 13:00:27 +0530

    path changes

--- src/header.jsx
+++ src/header.jsx
@@ -4,11 +4,9 @@ import {Link} from 'react-router-dom'
 function header() {
     return (
         <div className="header col-12 sm">
-            <Link className="header_logo" to='/home'>
+            <Link className="header_logo" to='/manoj-kumar-tadela/home'>
                 <span className="header_option1">🟢Manoj Kumar Tadela</span> 
             </Link>
-            
-
             
             <div className="header_nav">
                 <Link to='/details' ><div className="header_option"><span className="header_option1">About</span></div></Link>
