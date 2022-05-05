3e504406402938f4b89d36940d5f888ac89ebed8
MANOJ  TADELA <manojkumartadela333@gmail.com>
2022-05-05 12:36:45 +0530

    small nav changes

--- src/home.jsx
+++ src/home.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.css"
 import './home.css'
 import Paper from '@mui/material/Paper'
 import {Link} from 'react-router-dom'
-import { Button } from "@material-ui/core"
+import Button  from "@material-ui/core/Button"
 function home(){
     return (
     
@@ -23,7 +23,7 @@ function home(){
                 <h1 className="home_header" >Hi There..!</h1>
                 <p>I'm Manoj Kumar Tadela.</p>
                 <p>Passionate full-stack engineer with years of hands-on experience in developing scalable websites/applications using wide range of frot-end and back-end skills. </p> 
-                <Link to="/contact"><Button className="raised text-primary" >Contact Me</Button></Link>
+                <Link to="/contact"><Button className="contact" color="primary" variant="contained" >Contact Me</Button></Link>
             </div>
         </Paper>
         
