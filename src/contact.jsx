4fd54ceb7e2949e623a7da676c50ffe9d9b20d45
MANOJ  TADELA <manojkumartadela333@gmail.com>
2022-05-05 12:19:18 +0530

    Contact info

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import Card from "@mui/material/Card"
+import CallIcon from '@mui/icons-material/Call';
+import ContactMailRoundedIcon from '@mui/icons-material/ContactMailRounded';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import './contact.css'
 function contact() {
     return(
-        <div>some contact info</div>
+        <div className='card'>
+            <Card className='contact' elevation={5}>
+                <h2>
+                    <CallIcon color='primary' sx={{ fontSize: 40 }} />
+                    <span>9100526452</span>
+                </h2>
+                <h2>
+                    <ContactMailRoundedIcon color='primary' sx={{ fontSize: 40 }}/>
+                    <span><a href='https://mail.google.com/mail/u/0/#inbox'>manojkumartadela333@gmail.com</a></span>
+                </h2>
+                <h2>
+                    <LinkedInIcon color='primary' sx={{ fontSize: 40 }}/>
+                    <span><a href='https://www.linkedin.com/in/manoj-kumar-tadela-264056130/'>LinkedIn</a></span>
+                </h2>
+                <h2>
+                    <GitHubIcon color='primary' sx={{ fontSize: 40 }}/>
+                    <span><a href='https://github.com/tadelamanoj'>Git Hub</a></span>
+                </h2>
+                <h2>
+                    <FacebookRoundedIcon color='primary' sx={{ fontSize: 40 }}/>
+                    <span><a href='https://www.facebook.com/profile.php?id=100005478107549'>FaceBook</a></span>
+                </h2>
+                
+            </Card>
+        </div>
     )
 }
 export default contact
--- src/home.jsx
+++ src/home.jsx
@@ -6,7 +6,9 @@ import {
      } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.css"
 import './home.css'
-import Paper from '@mui/material/Paper';
+import Paper from '@mui/material/Paper'
+import {Link} from 'react-router-dom'
+import { Button } from "@material-ui/core"
 function home(){
     return (
     
@@ -21,6 +23,7 @@ function home(){
                 <h1 className="home_header" >Hi There..!</h1>
                 <p>I'm Manoj Kumar Tadela.</p>
                 <p>Passionate full-stack engineer with years of hands-on experience in developing scalable websites/applications using wide range of frot-end and back-end skills. </p> 
+                <Link to="/contact"><Button className="raised text-primary" >Contact Me</Button></Link>
             </div>
         </Paper>
         
