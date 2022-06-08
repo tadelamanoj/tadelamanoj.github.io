56fcb46442c4f89e8125dcabc9bcc4fbb150f01d
MANOJ  TADELA <manojkumartadela333@gmail.com>
2022-06-08 19:56:03 +0530

    aligned items center in contacts page

--- src/contact.jsx
+++ src/contact.jsx
@@ -3,30 +3,30 @@ import React from 'react';
 import CallIcon from '@mui/icons-material/Call';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import './contact.css'
 function contact() {
     return(
         <div className='card'>
             <div className='contact'>
-                <p>
+                <p id="lists">
                     <CallIcon color='primary' sx={{ fontSize: 40 }} />
-                    <span> 9100526452</span>
+                    <span id="lists">9100526452</span>
                 </p>
-                <p>
+                <p id="lists">
                     <EmailIcon color='primary' sx={{ fontSize: 40 }}/>
                     <span><a href='https://mail.google.com/mail/u/0/#inbox'> manojkumartadela333@gmail.com</a></span>
                 </p>
-                <p>
+                <p id="lists">
                     <LinkedInIcon color='primary' sx={{ fontSize: 40 }}/>
                     <span><a href='https://www.linkedin.com/in/manoj-kumar-tadela-264056130/'> LinkedIn</a></span>
                 </p>
-                {/* <p>
+                {/* <p id="lists">
                     <GitHubIcon color='black' sx={{ fontSize: 40 }}/>
                     <span><a href='https://github.com/tadelamanoj'> Git Hub</a></span>
                 </p> */}
-                <p>
+                <p id="lists">
                     <FacebookRoundedIcon color='primary' sx={{ fontSize: 40 }}/>
                     <span><a href='https://www.facebook.com/profile.php?id=100005478107549'> FaceBook</a></span>
                 </p>
