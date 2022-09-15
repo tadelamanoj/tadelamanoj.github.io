6269d872feb7bf6243623a801dcbf15db79ac221
tadelamanoj <manojkumartadela333@gmail.com>
2022-09-16 01:03:19 +0530

    Updated achievements and pics.

--- src/achievements.jsx
+++ src/achievements.jsx
@@ -5,11 +5,12 @@ function achievements() {
         <div className='achieve'>
             <div className='achieve_head'>Achievements</div>
                 <li><strong>CSE GATE</strong> qualified in 2021.</li>
+                <li>Solved more than 400+ DSA problems in various platforms like Leetcode, CodeChef, GeeksForGeeks, HackerEarth, HackerRank</li>
                 <li>Ranked below 1500 among 150000 which is top 1% in HackwithInfy conducted by Infosys in 2020.</li>
-                <li>Infosys certified MEAN Stack. </li>
+                <li>Infosys certified React.js,MongoDb,Node.js,Express.js,Angular Stack. </li>
                 <li>Certified in Data Mining from <strong> IIT Madras</strong>.</li> 
-                {/* <li>Won Prime Minister merit scholarship for a period of 4 years.</li>  */}
-                <li><strong>3* starter </strong>achieved in code chef.Active participant of competitive programming in code chef and leetcode.</li>  
+                <li>Recieved Prime Minister merit scholarship in B-Tech.</li> 
+                <li><strong>3* starter and progress to 5*</strong>achieved in code chef.Active participant of competitive programming in code chef and leetcode.</li>  
                 <li>Organizer of <strong>#code</strong> in Sri Venkateswara University College Of Engineering.</li> 
         </div>
     )
